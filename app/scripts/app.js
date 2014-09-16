965ab64d38f2d7893a56f23a641a4bbfddf4610d
pgruenbacher <pgruenbacher@gmail.com>
2014-09-16 16:48:56 -0400

    starting package

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bulletApp
+ * @description
+ * # bulletApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bulletApp', [
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
